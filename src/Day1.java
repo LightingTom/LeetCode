2c10759a21ef43005773a07030a40a2cf35db95c
Lenovo <1452442064@qq.com>
2020-06-16 22:52:20 +0800

    Day1 finished! (Add some comments)

--- src/Day1.java
+++ src/Day1.java
@@ -62,12 +62,15 @@ public class Day1 {
         return root;
     }
 
+    //Convert the binary tree to the specific format.
+    //For example,the following example is [1,2,3,null,null,4,5]
+    //Deserialize can convert the format string to a binary tree
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
-        root.right = new TreeNode(2);
-        root.right.left = new TreeNode(3);
-//        root.right.left = new TreeNode(4);
-//        root.right.right = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
         System.out.print(serialize(deserialize(serialize(root))));
     }
 }

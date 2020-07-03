715996b918b99189077261281462f2bb711408d5
Lenovo <1452442064@qq.com>
2020-07-03 09:58:59 +0800

    Day14 finished! Easy day!

---
+++ src/Day14.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Day14 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        if (nums.length == 1) return new TreeNode(nums[0]);
+        TreeNode root = new TreeNode(nums[nums.length/2]);
+        root.left = sortedArrayToBST(Arrays.copyOfRange(nums,0,nums.length/2));
+        root.right = sortedArrayToBST(Arrays.copyOfRange(nums,nums.length/2+1,nums.length));
+        return root;
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,37 +1,10 @@
+import java.util.Arrays;
+
 public class test {
-    public static int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        int left = matrix[0][0];
-        int right = matrix[n - 1][n - 1];
-        while (left < right) {
-            int mid = left + ((right - left) >> 1);
-            if (check(matrix, mid, k, n)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-
-    public static boolean check(int[][] matrix, int mid, int k, int n) {
-        int i = n - 1;
-        int j = 0;
-        int num = 0;
-        while (i >= 0 && j < n) {
-            if (matrix[i][j] <= mid) {
-                num += i + 1;
-                j++;
-            } else {
-                i--;
-            }
-        }
-        return num >= k;
-    }
-
     public static void main(String[] args) {
-        int[][] mat = {{1,5,9},{10,11,13},{12,13,15}};
-        System.out.print(kthSmallest(mat,6));
+        int[] a = {1,2,3,4};
+        int[] b = Arrays.copyOfRange(a,0,2);
+        System.out.print(b.length);
     }
 
 }

510c7351267f80ef939e4060b11761f72798dcbe
Lenovo <1452442064@qq.com>
2020-07-07 10:59:31 +0800

    Day18 finished! Tree traversal.

--- src/Day18.java
+++ src/Day18.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+
 public class Day18 {
+    static int summ = 0;
+    static ArrayList<Integer> values = new ArrayList<>();
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
         if (root.left == null && root.right == null) return root.val == sum;
         return hasPathSum(root.left,sum) || hasPathSum(root.right,sum);
+    }
+    public static boolean hasPathSum_bfs(TreeNode root, int sum){
+        if (root == null) return false;
+        bfs(root);
+        return values.contains(sum);
+    }
+    public static void bfs(TreeNode root){
+        if (root == null) return;
+        summ += root.val;
+        bfs(root.left);
+        bfs(root.right);
+        if (root.left == null && root.right == null) values.add(summ);
+        summ -= root.val;
     }
 }

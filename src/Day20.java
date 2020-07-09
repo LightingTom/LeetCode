fbe69751628964aaf45b0d683e39afd1c174d6a0
Lenovo <1452442064@qq.com>
2020-07-09 08:51:36 +0800

    Day20 finished! Trie(dictionary tree)

---
+++ src/Day20.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+public class Day20 {
+    public int respace(String[] dictionary, String sentence) {
+        int n = sentence.length();
+
+        Trie root = new Trie();
+        for (String word: dictionary) {
+            root.insert(word);
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = dp[i - 1] + 1;
+
+            Trie curPos = root;
+            //向前遍历，看这个单词在不在字典树中
+            for (int j = i; j >= 1; --j) {
+                int t = sentence.charAt(j - 1) - 'a';
+                if (curPos.next[t] == null) {
+                    break;
+                } else if (curPos.next[t].isEnd) {
+                    //相当于剪掉已识别的部分
+                    dp[i] = Math.min(dp[i], dp[j - 1]);
+                }
+                if (dp[i] == 0) {
+                    break;
+                }
+                curPos = curPos.next[t];
+            }
+        }
+        return dp[n];
+    }
+}
+
+//字典树
+class Trie{
+    public Trie[] next;
+    public boolean isEnd;
+
+    public Trie() {
+        next = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String s) {
+        Trie curPos = this;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            int t = s.charAt(i) - 'a';
+            if (curPos.next[t] == null) {
+                curPos.next[t] = new Trie();
+            }
+            curPos = curPos.next[t];
+        }
+        curPos.isEnd = true;
+    }
+}

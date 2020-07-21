347aa779821c02311337f6c662f20d1a584a693f
LightingTom <1452442064@qq.com>
2020-07-21 09:42:33 +0800

    Update the class: TreeNode

--- src/Day1.java
+++ src/Day1.java
@@ -79,8 +79,13 @@ class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-
+    TreeNode(){}
     TreeNode(int x) {
         val = x;
+    }
+    TreeNode(int x, TreeNode left, TreeNode right){
+        this.val = x;
+        this.left = left;
+        this.right = right;
     }
 }

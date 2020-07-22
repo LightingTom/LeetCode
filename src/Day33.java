62369190825f8400846099cabafa7f9837d2afcc
LightingTom <1452442064@qq.com>
2020-07-22 09:40:59 +0800

    Day33 finished! Rotate array

--- src/Day33.java
+++ src/Day33.java
@@ -1,4 +1,7 @@
 public class Day33 {
+    //pay attention: if we judge by right, we don't need to judge
+    //if it is a sorted array in advance, but if we use left to judge,
+    // it is necessary to do that
     public static int minArr(int[] numbers){
         int left = 0;
         int right = numbers.length-1;
